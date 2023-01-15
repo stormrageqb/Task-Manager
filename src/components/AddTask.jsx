02657c8ebca65252fb29a5246f9264b26ed7e156
stormrageqb <stormrage.qb@gmail.com>
2023-01-15 14:19:34 -0600

    Add AddTask component along with some initial styling and grid

---
+++ src/components/AddTask.jsx
@@ -0,0 +1,39 @@
+import styled from 'styled-components';
+import CheckMarkIcon from '/src/assets/icon-check.svg';
+
+const StyledArticle = styled.article`
+  grid-column: 1 / -1;
+  display: grid;
+  align-items: center;
+  grid-template-columns: 1fr 10fr 2fr;
+  padding: 1.5rem;
+  background-color: ${({ theme }) => theme.color.darkTheme.darkBlueDesat};
+  border-radius: 0.4rem;
+`;
+
+const StyledCheckMarkIcon = styled(CheckMarkIcon)``;
+
+const StyledInput = styled.input`
+  border: none;
+  font-family: inherit;
+  font-size: 1.6rem;
+  padding: 0.3rem 0;
+  background-color: ${({ theme }) => theme.color.darkTheme.darkBlueDesat};
+  color: ${({ theme }) => theme.color.lightTheme.lightGray};
+
+  &:focus {
+    border: none;
+    outline: none;
+  }
+`;
+
+const AddTask = () => {
+  return (
+    <StyledArticle>
+      <StyledCheckMarkIcon />
+      <StyledInput placeholder="Create a new todo..." />
+    </StyledArticle>
+  );
+};
+
+export default AddTask;
--- src/components/AppContainer.jsx
+++ src/components/AppContainer.jsx
@@ -1,12 +1,13 @@
 import styled from 'styled-components';
 import React from 'react';
 import SunIcon from '/src/assets/icon-sun.svg';
+import AddTask from './AddTask';
 
 const StyledSunIcon = styled(SunIcon)`
   fill: ${({ theme }) => theme.color.lightTheme.lightGray};
   /* height: 3rem;
   width: 3rem; */
-  transform: scale(0.85);
+  transform: scale(0.8);
 `;
 
 const StyledAppHeader = styled.header`
@@ -14,7 +15,7 @@ const StyledAppHeader = styled.header`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  background-color: blue;
+  background-color: transparent;
   & h1 {
     text-transform: uppercase;
     letter-spacing: 0.85rem;
@@ -27,12 +28,14 @@ const StyledAppHeader = styled.header`
 
 const StyledAppContainer = styled.div`
   grid-column: col-start 2 / col-start 4;
-  background-color: ${({ theme }) => theme.color.darkTheme.darkestGrayBlue};
+  /* background-color: ${({ theme }) =>
+    theme.color.darkTheme.darkestGrayBlue}; */
   display: grid;
   grid-template-columns: repeat(12, 1fr);
   grid-template-rows: min-content min-content min-content;
   height: 50rem;
   transform: translateY(-18rem);
+  row-gap: 2rem;
 `;
 
 const AppContainer = () => {
@@ -42,6 +45,7 @@ const AppContainer = () => {
         <h1>Todo</h1>
         <StyledSunIcon />
       </StyledAppHeader>
+      <AddTask />
     </StyledAppContainer>
   );
 };
