9be8627d78a54683cbdb67ba3019a69860efcf37
stormrageqb <stormrage.qb@gmail.com>
2023-01-16 22:29:59 -0600

    Clean up AppContainer component - exporting components related to AppHeader to AppHeader

--- src/components/AppContainer.jsx
+++ src/components/AppContainer.jsx
@@ -1,28 +1,8 @@
 import styled from 'styled-components';
 import React from 'react';
-import SunIcon from '/src/assets/icon-sun.svg';
 import AddTask from './AddTask';
 import TaskList from './TaskList';
 import AppHeader from './AppHeader';
-
-const StyledLogo = styled.h1`
-  text-transform: uppercase;
-  letter-spacing: 0.85rem;
-  font-size: 3rem;
-  color: ${({ theme }) => theme.color.lightTheme.lightGray};
-  line-height: 1;
-  padding-top: 0.2rem;
-`;
-
-const StyledSunIcon = styled(SunIcon)`
-  fill: ${({ theme }) => theme.color.lightTheme.lightGray};
-  transform: scale(0.8);
-  cursor: pointer;
-  transition: all 0.3s;
-  &:hover {
-    fill: ${({ theme }) => theme.color.lightTheme.grayBlue};
-  }
-`;
 
 const StyledAppContainer = styled.div`
   grid-column: col-start 2 / col-start 4;
@@ -44,10 +24,7 @@ const StyledAppContainer = styled.div`
 const AppContainer = () => {
   return (
     <StyledAppContainer>
-      <AppHeader>
-        <StyledLogo>Todo</StyledLogo>
-        <StyledSunIcon />
-      </AppHeader>
+      <AppHeader></AppHeader>
       <AddTask />
       <TaskList />
     </StyledAppContainer>
--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,3 +1,4 @@
+import SunIcon from '/src/assets/icon-sun.svg';
 import styled from 'styled-components';
 
 const StyledAppHeader = styled.header`
@@ -8,8 +9,32 @@ const StyledAppHeader = styled.header`
   background-color: transparent;
 `;
 
-const AppHeader = ({ children }) => {
-  return <StyledAppHeader>{children}</StyledAppHeader>;
+const StyledLogo = styled.h1`
+  text-transform: uppercase;
+  letter-spacing: 0.85rem;
+  font-size: 3rem;
+  color: ${({ theme }) => theme.color.lightTheme.lightGray};
+  line-height: 1;
+  padding-top: 0.2rem;
+`;
+
+const StyledSunIcon = styled(SunIcon)`
+  fill: ${({ theme }) => theme.color.lightTheme.lightGray};
+  transform: scale(0.8);
+  cursor: pointer;
+  transition: all 0.3s;
+  &:hover {
+    fill: ${({ theme }) => theme.color.lightTheme.grayBlue};
+  }
+`;
+
+const AppHeader = () => {
+  return (
+    <StyledAppHeader>
+      <StyledLogo>Todo</StyledLogo>
+      <StyledSunIcon />
+    </StyledAppHeader>
+  );
 };
 
 export default AppHeader;
