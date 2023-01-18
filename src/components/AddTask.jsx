d38249773787d66cff2e01e7aeb91d52d0a7ff32
stormrageqb <stormrage.qb@gmail.com>
2023-01-17 22:35:24 -0600

    Replace Checkbox component in input with CharacterCounter component - limit number of characters that can be entered for Todos in validation

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,9 +1,9 @@
 import { useState } from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import Button from './Button';
 import Checkbox from './Checkbox';
 import { StyledInput } from './styles/StyledInput.styled';
-
+const maximumCharacterValue = 45;
 const StyledForm = styled.form`
   grid-column: 1 / -1;
   display: grid;
@@ -16,6 +16,14 @@ const StyledForm = styled.form`
   box-shadow: rgba(0, 0, 0, 0.35) 0 2rem 3rem -1rem;
 `;
 
+const StyledCharacterCounter = styled.span`
+  font-size: 1.2rem;
+  font-weight: 400;
+  background-color: transparent;
+  margin-left: 0.3rem;
+  color: ${({ theme }) => theme.color.lightTheme.darkGrayBlue};
+`;
+
 const AddTask = ({ task, onAddTask, onChangeTask }) => {
   const [taskName, setTaskName] = useState('');
 
@@ -24,12 +32,22 @@ const AddTask = ({ task, onAddTask, onChangeTask }) => {
     onAddTask(taskName);
     setTaskName('');
   };
+
+  const handleTaskNameChange = e => {
+    if (e.target.value.length <= maximumCharacterValue) {
+      setTaskName(e.target.value);
+    }
+  };
   return (
     <StyledForm onSubmit={handleSubmit}>
-      <Checkbox onChange={onChangeTask} task={task} />
+      {/* <Checkbox onChange={onChangeTask} task={task} /> */}
+      <StyledCharacterCounter maximum={maximumCharacterValue}>
+        {maximumCharacterValue - taskName.length}
+      </StyledCharacterCounter>
       <StyledInput
         value={taskName}
-        onChange={e => setTaskName(e.target.value)}
+        // onChange={e => setTaskName(e.target.value)}
+        onChange={handleTaskNameChange}
         placeholder="Create a new todo..."
         aria-label="Create a new todo..."
         autoFocus
