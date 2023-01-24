364c04da902efca3c6d05f4e2589087bf18770c3
stormrageqb <stormrage.qb@gmail.com>
2023-01-23 21:31:50 -0600

    Wrap edit input and save button in form element so that edits can be submitted by hitting enter - change layout of editing state to include remaining character counter and better format the grid to display editing material

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -12,6 +12,12 @@ const StyledListItem = styled.li`
   font-weight: 400;
   display: grid;
   grid-template-columns: 1fr 10fr 1fr 1fr;
+  /* Change grid to allow form to fit - form is one child of grid including input and button - basically add 1fr to include the the input and save button in one grid column and subtract one from the end column */
+  ${props =>
+    props.isEditing &&
+    css`
+      grid-template-columns: 1fr 11fr 1fr;
+    `}
   align-items: center;
   padding: 1.5rem;
   background-color: ${({ theme }) => theme.color.darkTheme.darkBlueDesat};
@@ -23,6 +29,12 @@ const StyledListItem = styled.li`
     border-top-left-radius: 0.4rem;
     border-top-right-radius: 0.4rem;
   }
+`;
+
+const StyledInputForm = styled.form`
+  display: grid;
+  /* Add high fr number for input so it takes up the rest of the space when editing */
+  grid-template-columns: 10fr 1fr;
 `;
 
 const StyledTaskText = styled.span`
@@ -47,33 +59,41 @@ const Task = ({
   // const { showDeleteModal, setShowDeleteModal } = useContext(ModalContext);
   const [showDeleteModal, setShowDeleteModal] = useState(false);
 
+  const handleSubmit = e => {
+    e.preventDefault();
+    onSave();
+  };
   let taskContent;
 
   if (isEditing) {
     taskContent = (
       <>
-        <StyledInput
-          onChange={e => {
-            onChangeTask({
-              ...task,
-              text: e.target.value,
-            });
-          }}
-          editing
-          value={task.text}
-          autoFocus
-        />
-
-        <Button save onClick={onSave}>
-          Save
-        </Button>
+        {/* <span>20</span> */}
+        <StyledInputForm onSubmit={handleSubmit}>
+          <StyledInput
+            onChange={e => {
+              onChangeTask({
+                ...task,
+                text: e.target.value,
+              });
+            }}
+            editing
+            value={task.text}
+            autoFocus
+          />
+          <Button type="button" save onClick={onSave}>
+            Save
+          </Button>
+        </StyledInputForm>
       </>
     );
   } else {
     taskContent = (
       <>
         <StyledTaskText task={task}>{task.text}</StyledTaskText>
-        <Button onClick={onShowInput}>Edit</Button>
+        <Button type="button" onClick={onShowInput}>
+          Edit
+        </Button>
       </>
     );
   }
@@ -87,11 +107,13 @@ const Task = ({
           task={task}
         />
       )}
-      <StyledListItem>
-        <Checkbox task={task} onChange={onChangeTask} />
+      <StyledListItem isEditing={isEditing}>
+        {isEditing && <span>20</span>}
+        {!isEditing && <Checkbox task={task} onChange={onChangeTask} />}
+        {/* <Checkbox task={task} onChange={onChangeTask} /> */}
         {taskContent}
 
-        <Button onClick={() => setShowDeleteModal(true)}>
+        <Button type="button" onClick={() => setShowDeleteModal(true)}>
           <StyledTrashIcon />
         </Button>
       </StyledListItem>
