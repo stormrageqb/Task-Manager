1d59ebf59a0036e214a86b27d7bd913786becc8a
stormrageqb <stormrage.qb@gmail.com>
2023-01-24 10:54:06 -0600

    Export theme for ThemeProvider to its own file - import in App.js

---
+++ src/themes.js
@@ -0,0 +1,30 @@
+export const theme = {
+  // Fix the naming conventions when styles are actually applied
+  color: {
+    primary: '#3a7bfd',
+    secondary: '#c058f3',
+    trueGradient: ['rgba(87, 221, 255, 0.2)', 'rgba(192, 88, 243, 0.2)'],
+    modalGradient: ['rgba(87, 221, 255)', 'rgba(192, 88, 243)'],
+
+    lightTheme: {
+      lightGray: '#fafafa',
+      lightGrayBlue: '#e4e5f1',
+      grayBlue: '#d2d3db',
+      darkGrayBlue: '#9394a5',
+      darkestGrayBlue: '#484b6a',
+    },
+
+    darkTheme: {
+      darkBlue: '#161722',
+      darkBlueDesat: '#25273c',
+      lightGrayBlue: '#cacde8',
+      lightGrayBlueHover: '#e4e5f1',
+      darkGrayBlue: '#777a92',
+      darkerGrayBlue: '#4d5066',
+      darkestGrayBlue: '#393a4c',
+    },
+  },
+  font: {
+    primary: "'Josefin Sans', sans-serif",
+  },
+};
